9f9bec788b292e077a0d5e77a320da7b3b53d511
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-10-25 23:18:39 +0200

    zsh-autosuggestions

--- install.sh
+++ install.sh
@@ -112,8 +112,9 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 mkdir /.config/oh-my-zsh/custom/scripts
 
 # Oh-my-zsh addons
-cd ~/.config/oh-my-zsh/custom/enhancements/plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/enhancements/plugins/zsh-syntax-highlighting
+cd ~/.config/oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
 # Bat
