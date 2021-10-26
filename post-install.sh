5a2fbb7309e9175eea3046624083d112a5f2f46e
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-10-26 21:59:28 +0200

    Sensors

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,16 @@
+# Sensors
+sudo dnf install -y lm_sensors
+echo " 
+############################################################################################################################################
+
+# FINDING SENSORS. IT'S TAKE A TIME. PLEASE WAIT !
+
+############################################################################################################################################
+"
+yes | sudo sensors-detect
+
+# Spotify part 2
+echo " 
 ############################################################################################################################################
 
 # INSTALLING PACKEGE. IT'S TAKE A TIME. PLEASE WAIT !
@@ -6,3 +19,7 @@
 "
 sudo -u pkg-build lpf build spotify-client 
 sudo dnf install -y /var/lib/lpf/rpms/spotify-client/spotify-client-*.rpm
+
+
+# Reboot
+sudo reboot
