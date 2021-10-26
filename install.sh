a65592b7321bcda9eb5f0afea8e4857db42844b0
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-10-27 00:09:25 +0200

    VSCode

--- install.sh
+++ install.sh
@@ -139,10 +139,15 @@ sudo dnf copr enable -y  dusansimic/caprine
 sudo dnf update -y
 sudo dnf install -y caprine
 
-# Install VSCodium
-sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg 
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo 
-sudo dnf install -y codium 
+# Install VSCode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install -y code
+
+# Install VSCodium plugins
+code --install-extension esbenp.prettier-vscode
+code --install-extension redhat.vscode-xml  
 
 # Barshrc alias
 echo  >> ~/.bashrc
