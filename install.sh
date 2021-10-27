a20f2f06935e952303e88d02e2b117d3b7367784
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-10-27 10:43:31 +0200

    Vim-plug

--- install.sh
+++ install.sh
@@ -60,6 +60,11 @@ sudo dnf install -y gnome-tweaks
 
 # Neovim
 sudo dnf install -y neovim python3-neovim
+mkdir -p ~/.config/nvim
+
+# Vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Neofetch
 sudo dnf install -y neofetch
