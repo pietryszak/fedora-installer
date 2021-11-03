f040894ffb11b10c7d4a217c32e62ea1b692357f
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-03 21:59:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ sudo dnf install -y nodejs
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# FZF for neovim
+# FZF
 sudo dnf install -y  fzf
 
 # Bat - new cat ;)
