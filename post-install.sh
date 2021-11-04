67f6594426a063f4c688468ef75a5c1e77d47e0e
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-04 15:35:50 +0100

    Git clone my dotfiles

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,11 @@
+# My dotfiles
+cd ~/.gc/
+mkdir -p myconfig
+cd myconfig
+git clone https://github.com/pietryszak/dotfiles.git
+cd dotfiles 
+cd 
+
 # Sensors
 sudo dnf install -y lm_sensors
 echo " 
