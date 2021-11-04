d2f02024cfca99b46fa6f2f4a070f53a42212050
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-04 16:14:03 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -28,5 +28,13 @@ echo "
 "
 yes | DISPLAY= lpf update
 
+# Oh-my-zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Oh-my-zsh addons
+cd ~/.config/oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Reboot
 sudo reboot
