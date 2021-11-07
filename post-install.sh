f3cd5d719c5bccf5dffdf00f2f9f7fdf4c4ef133
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-07 13:59:02 +0100

    Code cleanup

--- post-install.sh
+++ post-install.sh
@@ -3,5 +3,8 @@ cd ~/.config/oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Sudo timeout back to default
+sudo sed -i 's/Defaults        env_reset,timestamp_timeout=60/#Defaults        env_reset,timestamp_timeout=60/g' /etc/default/grub
+
 # Reboot
 sudo reboot
