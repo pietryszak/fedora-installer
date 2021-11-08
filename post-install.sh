c4ee9f12c2c09eda86371e2fb896103bcbe035b2
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-08 16:15:48 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,14 @@ cd ~/.config/oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# My dotfiles
+cd ~/.gc/
+mkdir -p myconfig
+cd myconfig
+git clone https://github.com/pietryszak/dotfiles.git
+tar -xf Gruvbox.tar.gz -C ~/.local/share/icons/
+
+
 # Sudo timeout back to default
 sudo sed -i 's/Defaults        env_reset,timestamp_timeout=60/#Defaults        env_reset,timestamp_timeout=60/g' /etc/default/grub
 
