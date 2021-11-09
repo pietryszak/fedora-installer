4e1d7a28af9689d6ddb964d2cd15c4b0f19396ff
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-09 20:36:11 +0100

    Copy dotfiles

--- post-install.sh
+++ post-install.sh
@@ -5,14 +5,48 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # My dotfiles
 cd ~/.gc/
-mkdir -p myconfig
-cd myconfig
 git clone https://github.com/pietryszak/dotfiles.git
 
 # Copy icons from dotfiles to proper folder
 cd dotfiles/themes/
 mkdir -p ~/.local/share/icons
 tar -xf Gruvbox.tar.gz -C ~/.local/share/icons/
+
+# Copy bat  config to proper folder
+\cp -r ~/.gc/dotfiles/bat ~/.config
+
+# Copy bpytop config to proper folder
+\cp -r ~/.gc/dotfiles/bpytop ~/.config
+
+# Copy Code config to proper folder
+\cp -r ~/.gc/dotfiles/Code ~/.config
+
+# Copy htop config to proper folder 
+\cp -r ~/.gc/dotfiles/htop ~/.config
+
+# Copy ncspot config to proper folder
+\cp -r ~/.gc/dotfiles/ncspot ~/.config
+
+# Copy neofetch config to proper folder
+\cp -r ~/.gc/dotfiles/neofetch ~/.config
+
+# Copy nvim config to proper folder
+\cp -r ~/.gc/dotfiles/nvim ~/.config
+
+# Copy shortcuts list to proper folder
+\cp -r ~/.gc/dotfiles/shortcuts ~/.config
+
+# Copy spotifyd list to proper folder
+\cp -r ~/.gc/dotfiles/spotifyd ~/.config
+
+# Copy spotify-tui list to proper folder
+\cp -r ~/.gc/dotfiles/spotify-tui ~/.config
+
+# Copy sway list to proper folder
+\cp -r ~/.gc/dotfiles/sway ~/.config
+
+# Copy virtualbox list to proper folder
+\cp -r ~/.gc/dotfiles/virtualbox ~/.config
 
 
 # Sudo timeout back to default
