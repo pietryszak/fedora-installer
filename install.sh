850d7fb949c6b8ec33817bd15121ffcacf2414de
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-09 22:10:46 +0100

    Combine post install and install

--- install.sh
+++ install.sh
@@ -130,21 +130,6 @@ sudo systemctl enable clamav-freshclam
 # Firewalld GUI
 sudo dnf install -y firewall-config
 
-# Zsh
-sudo dnf install -y zsh
-
-# Powerlevel10k zsh
-cd ~/.gc
-mkdir fonts
-cd fonts
-wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf
-wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf
-wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf
-wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold%20Italic.ttf
-mkdir -p ~/.local/share/fonts/nerd
-cp MesloLGS* ~/.local/share/fonts/nerd
-fc-cache -fv
-
 # Bat
 sudo dnf install -y bat
 
@@ -177,7 +162,7 @@ sudo modprobe vboxdrv
 
 # Virtualbox extensions pack
 cd ~/.gc
-mkdir -p virtualbox
+mkdir -p VirtualBox
 cd virtualbox
 LatestVirtualBoxVersion=$(wget -qO - https://download.virtualbox.org/virtualbox/LATEST-STABLE.TXT) && wget "https://download.virtualbox.org/virtualbox/${LatestVirtualBoxVersion}/Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack"
 yes | sudo VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack
@@ -284,5 +269,81 @@ sudo dnf autoremove -y
 sudo dnf update -y
 sudo dnf upgrade -y
 
-# Oh-my-zsh ALWAYS AS LAST
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Oh-my-zsh
+ZSH="$HOME/.gc/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Oh-my-zsh addons
+cd ~/.gc/oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.gc/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.gc/oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Powerlevel10k zsh
+cd ~/.gc
+mkdir fonts
+cd fonts
+wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf
+wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf
+wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf
+wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold%20Italic.ttf
+mkdir -p ~/.local/share/fonts/nerd
+cp MesloLGS* ~/.local/share/fonts/nerd
+fc-cache -fv
+cd ~/.gc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.gc/oh-my-zsh/custom}/themes/powerlevel10k
+
+# My dotfiles
+cd ~/.gc/
+git clone https://github.com/pietryszak/dotfiles.git
+
+# Copy icons from dotfiles to proper folder
+cd dotfiles/themes/
+mkdir -p ~/.local/share/icons
+tar -xf Gruvbox.tar.gz -C ~/.local/share/icons/
+
+# Copy bat  config to proper folder
+\cp -r ~/.gc/dotfiles/bat ~/.config
+
+# Copy bpytop config to proper folder
+\cp -r ~/.gc/dotfiles/bpytop ~/.config
+
+# Copy Code config to proper folder
+\cp -r ~/.gc/dotfiles/Code ~/.config
+
+# Copy htop config to proper folder 
+\cp -r ~/.gc/dotfiles/htop ~/.config
+
+# Copy ncspot config to proper folder
+\cp -r ~/.gc/dotfiles/ncspot ~/.config
+
+# Copy neofetch config to proper folder
+\cp -r ~/.gc/dotfiles/neofetch ~/.config
+
+# Copy nvim config to proper folder
+\cp -r ~/.gc/dotfiles/nvim ~/.config
+
+# Copy shortcuts list to proper folder
+\cp -r ~/.gc/dotfiles/shortcuts ~/.config
+
+# Copy spotifyd list to proper folder
+\cp -r ~/.gc/dotfiles/spotifyd ~/.config
+
+# Copy spotify-tui list to proper folder
+\cp -r ~/.gc/dotfiles/spotify-tui ~/.config
+
+# Copy sway list to proper folder
+\cp -r ~/.gc/dotfiles/sway ~/.config
+
+# Copy virtualbox list to proper folder
+\cp -r ~/.gc/dotfiles/virtualbox ~/.config
+
+# Copy zsh list to proper folder
+\cp -r ~/.gc/dotfiles/zsh ~/.config
+
+# Copy zshrc list to proper folder
+\cp -r ~/.config/zsh/.zshrc ~/
+
+# Sudo timeout back to default
+sudo sed -i 's/Defaults        env_reset,timestamp_timeout=60/#Defaults        env_reset,timestamp_timeout=60/g' /etc/default/grub
+
+# Reboot
+sudo reboot
