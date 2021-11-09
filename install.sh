719eb91f241aa732dffe363d4167fdc36df1bd83
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-09 22:53:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -74,7 +74,10 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # FZF
-sudo dnf install -y  fzf
+cd ~/.gc
+git clone --depth 1 https://github.com/junegunn/fzf.git
+cd fzf
+yes | ./install
 
 # Bat - new cat ;)
 sudo dnf install -y bat
