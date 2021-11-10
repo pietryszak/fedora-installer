e57b855335d5ddd8fb6a04a08df91a3edb59976e
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-10 22:58:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -221,6 +221,10 @@ sudo dnf install -y spotify-tui
 echo  >> ~/.bashrc
 echo alias vim='nvim' >> ~/.bashrc
 echo alias vi='nvim' >> ~/.bashrc
+
+# Bash only checks the first word of a command for an alias, any words after that are not checked. That means in a command like sudo ll, only the first word (sudo) is checked by bash for an alias, ll is ignored. We can tell bash to check the next word after the alias (i.e sudo) by adding a space to the end of the alias value.
+echo alias sudo='sudo ' >> ~/.bashrc
+
 source ~/.bashrc
 sudo bash -c "sudo echo  >> /root/.bashrc"
 sudo bash -c "sudo echo alias vim='nvim' >> /root/.bashrc"
