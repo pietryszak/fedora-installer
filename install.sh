a2bdb9e1be8621ae2e7d8ecb0202bd416392c4ba
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-10 16:19:28 +0100

    Code cleanup

--- install.sh
+++ install.sh
@@ -252,8 +252,6 @@ cd ~/.gc
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
 
-
-
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
