dd4034107ada019307119d20ae3b11f199594ccc
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2021-11-28 21:04:09 +0100

    Update vm-vmware-install.sh

--- vm-vmware-install.sh
+++ vm-vmware-install.sh
@@ -1,1 +1,7 @@
+# Replace polybar config for VmWare
+rm ~/.config/polybar/cuts/config.ini
+mv ~/.config/polybar/cuts/vm-config.ini ~/.config/polybar/cuts/config.ini
 
+# Replace i3-gaps workspaces for VmWare
+rm ~/.config/i3/scripts/workspaces
+mv ~/.config/i3/scripts/vmware-workspaces ~/.config/i3/scripts/workspaces
