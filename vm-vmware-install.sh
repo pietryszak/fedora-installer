1221c88a74f54a9febfc856fa1aa432fcec074cf
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2022-04-19 20:56:59 +0200

    Change i3 scripts for VmWare

--- vm-vmware-install.sh
+++ vm-vmware-install.sh
@@ -5,6 +5,3 @@ mv ~/.config/polybar/cuts/vmware-config.ini ~/.config/polybar/cuts/config.ini
 # Replace i3-gaps workspaces for VmWare
 rm ~/.config/i3/scripts/workspaces
 mv ~/.gc/dotfiles/i3/scripts/vmware-workspaces ~/.config/i3/scripts/workspaces
-
-# Replace kitty to terminator
-sudo sed -i 's/bindsym $mod+Return exec --no-startup-id kitty/bindsym $mod+Return exec --no-startup-id terminator --no-dbus/g' ~/.config/i3/scripts/bindings
