314bea4a957b8f7b4cf90eccd6c11c43069e4635
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2022-05-30 19:14:02 +0200

    Installation of virtualbox additions

--- install.sh
+++ install.sh
@@ -193,7 +193,9 @@ echo "${green}${bold}INSTALLING VIRTUALBOX EXTENSION PACK${reset}"
 cd ~/.gc
 mkdir -p VirtualBox
 cd VirtualBox
-LatestVirtualBoxVersion=$(wget -qO - https://download.virtualbox.org/virtualbox/LATEST-STABLE.TXT) && wget -q "https://download.virtualbox.org/virtualbox/${LatestVirtualBoxVersion}/Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack" 
+LatestVirtualBoxVersion=$(wget -qO - https://download.virtualbox.org/virtualbox/LATEST-STABLE.TXT) && wget "https://download.virtualbox.org/virtualbox/${LatestVirtualBoxVersion}/Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack" &&
+yes | sudo VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack &&
+cd
 
 # Virtualbox NAT Network nad Host-only Network
 echo "${green}${bold}ADDING VIRTUALBOX NAT AND HOST-ONLY NETWORKS${reset}"
